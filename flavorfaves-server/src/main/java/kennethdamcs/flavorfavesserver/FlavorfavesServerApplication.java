c6630341b5c50edb222e72ead429ce3f48251ca1
KennethDamCS <mrdam.kiet@gmail.com>
2024-01-11 16:02:18 -0800

    Front end and back end file creation

---
+++ flavorfaves-server/src/main/java/kennethdamcs/flavorfavesserver/FlavorfavesServerApplication.java
@@ -0,0 +1,13 @@
+package kennethdamcs.flavorfavesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorfavesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlavorfavesServerApplication.class, args);
+    }
+
+}
---
+++ flavorfaves-server/src/test/java/kennethdamcs/flavorfavesserver/FlavorfavesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kennethdamcs.flavorfavesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavorfavesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
